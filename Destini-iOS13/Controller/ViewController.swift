d4aa241a5cbb5902dde68a8ad62a1b5a021502d8
Mrinalini Upadhya <MrinaliniU@users.noreply.github.com>
2023-12-30 16:39:38 -0800

    my solution

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateStory(storyBrain.getCurrentStory())
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        let currentStory = storyBrain.getCurrentStory()
+        updateStory(storyBrain.getNextStory(currentStory, userChoice))
+    }
+    
+    func updateStory(_ story: Story) {
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for: .normal)
+        choice2Button.setTitle(story.choice2, for: .normal)
+    }
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+    var choice1Destination: Int
+    var choice2Destination: Int
+    
+    init(_ title: String, _ choice1: String, _ choice1Destination: Int, _ choice2: String, _ choice2Destination: Int) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
+    }
+}
